bf20e90ee4eef40b51182870af6a16b40feb4ee4
Steve-Paytosh <mrblond619f5@hotmail.com>
2016-08-01 02:33:41 -0400

    added code that rejects pasting to the text box and detects a winner

--- Type Set Fail/script.js
+++ Type Set Fail/script.js
@@ -1,14 +1,24 @@
 var input="";
 var current_word="cat";
-var _MAX_SIZE=5;
+var _MAX_SIZE=15;
+var match=0;
+var phase=2; // phase 0=welcome, phase 1= info, phase 2=gameplay, phase 3=round over
+var paste=false;
 
 $(document).ready(function(){
 	
-	var phase=0; // phase 0=welcome, phase 1= info, phase 2=gameplay, phase 3=round over
+	
   
 	
 		$(".text-box").focus( function() 
 		{
+			
+			document.onpaste=function(e){
+				e.preventDefault();
+				//$("#test").html("CHEATER!!!");
+				document.getElementsByClassName("text-box")[0].value="NO CHEATING!!!";
+				paste=true;
+			}
 			
 			document.onkeyup=function(){
 				$(".text-box").blur();
@@ -17,6 +27,11 @@ $(document).ready(function(){
 			
 			if(document.getElementsByClassName("text-box")[0].value != input)
 				letterPress();
+			
+			if(match)
+				win();
+			
+			
 		}
 	);
 	
@@ -25,6 +40,21 @@ $(document).ready(function(){
 
 function letterPress()
 {
+	if(phase!=2)
+	{
+		document.getElementsByClassName("text-box")[0].value="";
+		input=text_box_value;
+		$(".text-box").disabled=true;
+		//$(".text-box").hide
+		return;
+	}
+	
+	if(paste)
+	{
+		paste=false;
+		return;
+	}
+	
 	//get the newest letter typed
 	var text_box_value=document.getElementsByClassName("text-box")[0].value;
 	var text_box_length=text_box_value.length;
@@ -35,15 +65,31 @@ function letterPress()
 	if(text_box_value.length>_MAX_SIZE)
 	{
 		var temp=text_box_value.slice(text_box_length-_MAX_SIZE,text_box_length);
-		document.getElementsByClassName("text-box")[0].value=temp;
+		//document.getElementsByClassName("text-box")[0].value=temp;
 		text_box_value=temp;
 		
 	}
 	
-	
-	//input=text_box_value;
-	//input.toUpperCase();
 	text_box_value=text_box_value.toUpperCase();
 	document.getElementsByClassName("text-box")[0].value=text_box_value;
+	input=text_box_value;
 	
+	//check for a match
+	
+	if(text_box_value.includes(current_word.toUpperCase()) )
+	{
+			match=1;
+	}
 }
+
+function win()
+{
+	$("#test").html("winner!");
+	phase=3;
+}
+
+function lose(){
+	$("#test").html("loser!");
+	phase=3;
+}
+
