836b27cd8665907fd0fa226a7d8eae8ad6b1a26a
Steve-Paytosh <mrblond619f5@hotmail.com>
2016-08-01 01:26:52 -0400

    game forces text to be uppercase and trims to a max size

--- Type Set Fail/script.js
+++ Type Set Fail/script.js
@@ -1,4 +1,6 @@
 var input="";
+var current_word="cat";
+var _MAX_SIZE=5;
 
 $(document).ready(function(){
 	
@@ -13,7 +15,6 @@ $(document).ready(function(){
 				$(".text-box").focus();
 			};
 			
-			
 			if(document.getElementsByClassName("text-box")[0].value != input)
 				letterPress();
 		}
@@ -24,8 +25,25 @@ $(document).ready(function(){
 
 function letterPress()
 {
-	var x=document.getElementsByClassName("text-box")[0].value;
-	var l=x.length;
-	$("#test").html(x.charAt(l-1));
-	input=x;
+	//get the newest letter typed
+	var text_box_value=document.getElementsByClassName("text-box")[0].value;
+	var text_box_length=text_box_value.length;
+	var new_char=text_box_value.charAt(text_box_length-1); //letter typed
+	$("#test").html(new_char);
+	
+	//truncate the text box value if it exceeds the maximum character size
+	if(text_box_value.length>_MAX_SIZE)
+	{
+		var temp=text_box_value.slice(text_box_length-_MAX_SIZE,text_box_length);
+		document.getElementsByClassName("text-box")[0].value=temp;
+		text_box_value=temp;
+		
+	}
+	
+	
+	//input=text_box_value;
+	//input.toUpperCase();
+	text_box_value=text_box_value.toUpperCase();
+	document.getElementsByClassName("text-box")[0].value=text_box_value;
+	
 }
